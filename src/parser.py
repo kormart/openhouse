6969c7a0a609e7825f0574232ea02c63e4123b38
kormart <martin.korling@ri.se>
2022-04-27 10:54:09 +0200

    update parser

--- src/parser.py
+++ src/parser.py
@@ -1,14 +1,15 @@
-from cmath import inf
 import re    
+
+# prototype:
 # raw = ' Some text\n\nABSTRACT\nExtract \nthis text\nAbstract: \nFunny \n\nOther text'
 # pattern = 'abstract:|abstract'
+# abstract = re.split(pattern ,raw, flags=re.IGNORECASE)[2].split("\n\n")[0]
 
 result = []
 
 with open('src/posters.txt', 'r') as infile:
     raw = infile.read()
 
-# abstract = re.split(pattern ,raw, flags=re.IGNORECASE)[2].split("\n\n")[0]
 title_pattern = 'title:'
 abstracts = re.split(title_pattern, raw, flags=re.IGNORECASE)[1:]
 # print(len(abstracts))
